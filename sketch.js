a3343d8a489bbf52eca97a19568ccf64e1d95751
YuriiHoliuk <bionicuss@gmail.com>
2017-03-23 18:12:02 +0200

    add system selector

--- sketch.js
+++ sketch.js
@@ -63,13 +63,29 @@ var systems = {
 
 // var currentSystem = systems.koch_curve; //Why it doesent work?
 
-var axiom = systems.sierpinski_triangle.axiom;
-var string = axiom;
-var rules = systems.sierpinski_triangle.rules;
-var turtle = systems.sierpinski_triangle.turtle;
-var len = systems.sierpinski_triangle.len;
-var angle = systems.sierpinski_triangle.angle;
+var currentSystem = document.querySelector('.system').value;
 
+var axiom = systems[currentSystem].axiom;
+var string = axiom;
+var rules = systems[currentSystem].rules;
+var turtle = systems[currentSystem].turtle;
+var len = systems[currentSystem].len;
+var angle = systems[currentSystem].angle;
+
+function setSystem() {
+    currentSystem = document.querySelector('.system').value;
+
+    axiom = systems[currentSystem].axiom;
+    string = axiom;
+    rules = systems[currentSystem].rules;
+    turtle = systems[currentSystem].turtle;
+    len = systems[currentSystem].len;
+    angle = systems[currentSystem].angle;
+
+    background(51);
+    createP(axiom);
+    turtle(axiom);
+}
 
 function generate() {
     var newString = '';
@@ -98,8 +114,11 @@ function generate() {
 function setup() {
     createCanvas(1000, 1000);
     background(51);
+
     var button = createButton('Generate').addClass('generate');
     button.mousePressed(generate);
+    document.querySelector('.system').addEventListener('change', setSystem);
+
     createP(axiom);
     turtle(axiom);
 }
